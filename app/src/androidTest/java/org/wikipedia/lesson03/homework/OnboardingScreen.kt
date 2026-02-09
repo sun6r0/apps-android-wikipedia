726d1e567a5a2dabcc874111ef9a0ba47f7aa089
sun6r0 <dshvaiko7@gmail.com>
2026-02-09 18:56:52 +0300

    lesson03

---
+++ app/src/androidTest/java/org/wikipedia/lesson03/homework/OnboardingScreen.kt
@@ -0,0 +1,37 @@
+package org.wikipedia.lesson03.homework
+
+import com.google.android.material.button.MaterialButton
+import org.wikipedia.R
+
+val one = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button
+)
+val two = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_done_button
+)
+val three = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_forward_button
+)
+val four = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_pager_container
+)
+val five = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button
+)
+val six = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button
+)
+val seven = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button
+)
+val eight = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button
+)
